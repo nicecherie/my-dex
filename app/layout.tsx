00cfa3226ad48e3a9afa09fd700b31f2d77eb1cc
nicecherie <huangcherie@163.com>
2025-11-17 17:10:32 +0800

    new: 全局字体设置

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,17 @@
 import type { Metadata } from 'next'
+import { Inter, JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 
+const inter = Inter({ variable: '--font-inter', subsets: ['latin'] })
+const jetBrainsMono = JetBrains_Mono({
+  variable: '--font-jetBrainsMono',
+  subsets: ['latin']
+})
+
 export const metadata: Metadata = {
-  title: 'MySwap'
+  title: 'MySwap',
+  description: '基于区块链的去中心化代币交换平台'
 }
 
 export default function RootLayout({
@@ -12,8 +20,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className="">{children}</body>
+    <html lang="zh-CN" suppressHydrationWarning>
+      <body
+        className={`${inter.variable} ${jetBrainsMono.variable}
+        antialiased bg-background text-foreground min-h-screen font-sans
+      `}
+      >
+        {children}
+      </body>
     </html>
   )
 }
